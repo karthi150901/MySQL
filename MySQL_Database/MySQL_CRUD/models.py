31272b522152bcd2e01b0f88b224bd73c59b03dd
Karthikeyan <karthikeyan15092001@gmail.com>
2023-10-04 21:57:00 +0530

    initial

---
+++ MySQL_Database/MySQL_CRUD/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class RegisterForm(models.Model):
+    name= models.CharField(max_length=100)
+    age= models.IntegerField()
+    address= models.CharField(max_length=100)
+    contact= models.CharField(max_length=15)
+    email= models.EmailField()
+    
+
+
+    # class Meta:
+    #     db_table= 'datas'
+
+    class Meta:
+        ordering=['name']
+    
